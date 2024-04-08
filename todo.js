af6739b51a384fd290e3bc1ebd0e78a7f329f5b1
Sabrina-Tseng <tsens543@newschool.edu>
2024-04-08 14:11:01 -0400

    Working countdown timer

--- todo.js
+++ todo.js
@@ -1,21 +1,87 @@
-//database array
-const todoItems = [];
-
 //find in html
 const dotolist = document.getElementById('todo-list');
 const form =  document.getElementById('form');
 const addButton = document.getElementById('add-button');
 
-//set up swap method
-// Array.prototype.swap = function(a, b){
-// 	this[a] = this.splice(b, 1, this[a])[0];
-// 	return this;
+const globalCountdown = document.getElementById('global-countdown');
+const planTime = document.querySelector('input[type="time"]');
+// const startButton = document.getElementById('start-button');
+
+
+//========== countdown timer ==========
+
+//count down
+let timer;
+function startTimer(){
+
+	//reset timer
+	clearInterval(timer);
+
+	//set time on the same day
+	let timeNow = new Date();
+	let monthNow = timeNow.getMonth()+1
+	let dateNow = timeNow.getDate()
+	let yearNow = timeNow.getFullYear()
+
+	let timeEnd = new Date( monthNow + '/' + dateNow + '/' + yearNow + ' ' + planTime.value);
+
+	//if set time is earlier than current time, set to next day
+	if (timeNow > timeEnd) {
+		timeEnd.setDate(timeEnd.getDate() + 1)
+	}
+
+	timer = setInterval(function() {
+
+		// Get today's date and time
+		let now = new Date().getTime();
+
+		// Find the distance between now and the count down date
+		let distance = timeEnd.getTime() - now;
+
+		// Time calculations for days, hours, minutes and seconds
+		let days = Math.floor(distance / (1000 * 60 * 60 * 24));
+		let hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+		let minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+		let seconds = Math.floor((distance % (1000 * 60)) / 1000);
+
+		// Display the result in the element with id="demo"
+		globalCountdown.innerHTML = days + "d " + hours + "h "
+		+ minutes + "m " + seconds + "s ";
+
+		// If the count down is finished, write some text
+		if (distance < 0) {
+			clearInterval(timer);
+			// globalCountdown.innerHTML = "EXPIRED";
+			document.getElementById('countdown-msg').innerHTML = 'You are late!'
+		}
+
+	}, 1000);
+}
+
+//start timer
+// startButton.onclick = (event) => {
+// 	event.preventDefault();
+// 	startTimer()
 // }
+
+//when changed, save in localstorage and start the timer wit the new time
+planTime.oninput = () => {
+	storePlanTime(planTime.value);
+	startTimer();
+}
+// Save to localStorage
+function storePlanTime(input) {
+	localStorage.setItem("planTimeStorage", input);
+}
+
+//========== todo list ==========
+
+//set up swap method
 Array.prototype.swap = function(a, b) {
 	var temp = this[a];
 	this[a] = this[b];
 	this[b] = temp;
-  };
+};
 
 //object constructor
 class task {
@@ -31,14 +97,12 @@ function addNewTask(name,duration){
 	todoItems.push(newTask);
 }
 
-//default list
-addNewTask('brush teeth',3);
-addNewTask('use toilet',5);
-addNewTask('get dressed',10);
-
-
 //refresh todolist
 function displayTask(){
+
+	//save to local storage
+	storeTodoList();
+
 	let list = '';
 
 	todoItems.forEach(item =>{
@@ -96,7 +160,6 @@ function displayTask(){
 	}
 
 }
-displayTask();
 
 //jquery sortable list
 // !function(a){function f(a,b){if(!(a.originalEvent.touches.length>1)){a.preventDefault();var c=a.originalEvent.changedTouches[0],d=document.createEvent("MouseEvents");d.initMouseEvent(b,!0,!0,window,1,c.screenX,c.screenY,c.clientX,c.clientY,!1,!1,!1,!1,0,null),a.target.dispatchEvent(d)}}if(a.support.touch="ontouchend"in document,a.support.touch){var e,b=a.ui.mouse.prototype,c=b._mouseInit,d=b._mouseDestroy;b._touchStart=function(a){var b=this;!e&&b._mouseCapture(a.originalEvent.changedTouches[0])&&(e=!0,b._touchMoved=!1,f(a,"mouseover"),f(a,"mousemove"),f(a,"mousedown"))},b._touchMove=function(a){e&&(this._touchMoved=!0,f(a,"mousemove"))},b._touchEnd=function(a){e&&(f(a,"mouseup"),f(a,"mouseout"),this._touchMoved||f(a,"click"),e=!1)},b._mouseInit=function(){var b=this;b.element.bind({touchstart:a.proxy(b,"_touchStart"),touchmove:a.proxy(b,"_touchMove"),touchend:a.proxy(b,"_touchEnd")}),c.call(b)},b._mouseDestroy=function(){var b=this;b.element.unbind({touchstart:a.proxy(b,"_touchStart"),touchmove:a.proxy(b,"_touchMove"),touchend:a.proxy(b,"_touchEnd")}),d.call(b)}}}(jQuery);
@@ -105,33 +168,73 @@ displayTask();
 //   } );
 
 //add new task
-addButton.addEventListener("click",function(e){
+addButton.onclick = () => {
+
+	//hide add button
 	addButton.classList.add("hidden");
+
+	//show add form
+	//for, id, and name should match
 	form.innerHTML = `
-		<form name="add-new-task" id="add-form">
-			<div>
+		<form name="add-form" id="add-form">
+			<section>
 				<label for="task">Task</label><br>
 				<input type="text" id="task" name="task" required><br>
 				<label for="time">Duration</label><br>
 				<input type="number" id="time" name="time" min="1" required> minutes
-			</div>
+			</section>
 			<button type="submit">Add</button>
 		</form>
 	`
 	const addform =  document.getElementById('add-form');
 
-	addform.addEventListener('submit', function(e){
-		e.preventDefault();
+	//submit new task
+	addform.onsubmit = (event) => {
+		event.preventDefault();
 	
 		const taskInput = addform.elements.task;
 		const timeInput = addform.elements.time;
 		
 		if (taskInput.value != '' && timeInput.value > 0) {
-			console.log("add");
+			// console.log("add");
 			addNewTask(taskInput.value, timeInput.value);
 			displayTask();
+
+			//hide add form and show add button
 			form.innerHTML = "";
 			addButton.classList.remove("hidden");
 		};
-	})
-});
+	};
+};
+
+// Save to localStorage
+function storeTodoList() {
+	localStorage.setItem("todoItemStorage", JSON.stringify(todoItems));
+}
+
+//========== on load ==========
+
+// Check for todo list in localStorage
+
+//empty database array
+let todoItems = [];
+
+//check if there's local storage
+if (localStorage.length > 0) {
+	//list
+	todoItems = JSON.parse(localStorage.getItem('todoItemStorage')); // Update the form from localStorage
+	displayTask();
+	//time
+	planTime.value = localStorage.getItem('planTimeStorage');
+} else {
+	//default list
+	addNewTask('Brush teeth',3);
+	addNewTask('Use toilet',5);
+	addNewTask('Get dressed',10);
+	displayTask();
+	//default time
+	planTime.value = "09:00";
+	storePlanTime(planTime.value);
+}
+
+startTimer();
